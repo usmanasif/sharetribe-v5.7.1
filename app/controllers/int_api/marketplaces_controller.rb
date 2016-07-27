cf8374465d8eff0e78f378467cb2b6609aff8032
im genius <im.genius.man@gmail.com>
2016-07-27 13:41:52 +0500

    SSO Update

--- app/controllers/int_api/marketplaces_controller.rb
+++ app/controllers/int_api/marketplaces_controller.rb
@@ -1,6 +1,7 @@
 class IntApi::MarketplacesController < ApplicationController
 
-  #skip_filter :fetch_community, :check_http_auth
+  skip_filter :fetch_community, :check_http_auth, :only => [:login]
+
 
   before_filter :set_access_control_headers
 
